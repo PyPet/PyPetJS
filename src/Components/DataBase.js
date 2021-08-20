eadc7660602112d41b29dc12e483fd0a5ab07b4a
Restyled.io <commits@restyled.io>
2021-08-20 20:19:28 +0000

    Restyled by prettier

--- src/Components/DataBase.js
+++ src/Components/DataBase.js
@@ -1,14 +1,18 @@
-import {createClient} from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
 
 function Database(petid = process.env.petid) {
-  const supabase =
-      createClient(process.env.supabase_url, process.env.supabase_anon);
+  const supabase = createClient(
+    process.env.supabase_url,
+    process.env.supabase_anon
+  );
   function getPetConfig() {
-    const {data, error} =
-        supabase.from("pets").select().textSearch("id", petid);
-    return {data, error};
+    const { data, error } = supabase
+      .from("pets")
+      .select()
+      .textSearch("id", petid);
+    return { data, error };
   }
 }
 
